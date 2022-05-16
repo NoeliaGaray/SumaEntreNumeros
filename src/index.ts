6730dad5898bf93511e4987a46af4417c7d3a064
NoeliaGaray <103612855+NoeliaGaray@users.noreply.github.com>
2022-05-15 22:44:00 -0300

    kalsdjklas

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
-var número1: number = 0;
-var número2: number = 0;
+var numero1: number = 0;
+var numero2: number = 0;
 var resultado: number = 0;
 // var suma: number = 0;
 
-número1 = Number(prompt("Ingrese un número entero"));
-número2 = Number(prompt("Ingrese otro número entero"));
+numero1 = Number(prompt("Ingrese un número entero"));
+numero2 = Number(prompt("Ingrese otro número entero"));
 
-for (número1 = 0; número1 <= número2; número1++) {
-  número1 += número2;
-  resultado = número1 + número2;
+for (let i: number = numero1; i <= numero2; i++) {
+  resultado = resultado + i;
 }
 console.log("La suma entre los números es: ", resultado);
